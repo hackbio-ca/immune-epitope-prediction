1541e34a66657bc8370ad482ccb4b8db1d513ed6
Paulo Miguel <paulomiguel@Epitopea-L031.local>
2025-09-21 10:19:56 -0400

    modify: add argument parsing to laoding_data python util

--- utils/loading_data.py
+++ utils/loading_data.py
@@ -1,30 +1,56 @@
 import pandas as pd
 import torch
+import argparse
 
-df = pd.read_csv(
-    "test.csv",
-    sep=r"\s+",  # split on any whitespace
-    skiprows=2,  # skip the first two lines
-    header=None,  # no header
-    names=[
-        "index",
-        "amino_acid",
-        "prediction_score",
-        "sequence_id",
-        "extra1",
-        "extra2",
-    ],
-)
 
-# Keep only the columns we care about
-df = df[["amino_acid", "prediction_score"]]
+def get_cli_args():
 
-# Convert scores to float
-df["prediction_score"] = df["prediction_score"].astype(float)
+    parser = argparse.ArgumentParser(
+        description='Extract peptides from NetChop results'
+    )
 
-# Convert to numpy / torch
-amino_acids = df["amino_acid"].astype(str).values
-scores = torch.tensor(df["prediction_score"].values, dtype=torch.float32).cuda()
+    parser.add_argument('-f', '--filename',
+        required=True,
+        type=str,
+        help='Input results file from NetMHCpan path and name'                    
+        )
+    
+    parser.add_argument('-o', '--output',
+        required=True,
+        type=str,
+        help='Output file path and name')
+    
+    return parser.parse_args()
+
+
+def read_results(filename):
+    
+    df = pd.read_csv(
+        filename,
+        sep=r"\s+",  # split on any whitespace
+        skiprows=2,  # skip the first two lines
+        header=None,  # no header
+        names=[
+            "index",
+            "amino_acid",
+            "prediction_score",
+            "sequence_id",
+            "extra1",
+            "extra2",
+        ],
+    )
+
+    # Keep only the columns we care about
+    df = df[["amino_acid", "prediction_score"]]
+
+    # Convert scores to float
+    df["prediction_score"] = df["prediction_score"].astype(float)
+
+    # Convert to numpy / torch
+    amino_acids = df["amino_acid"].astype(str).values
+    scores = torch.tensor(df["prediction_score"].values, dtype=torch.float32).cuda()
+
+    return amino_acids, scores
 
 
 # Peptide extraction function
@@ -47,18 +73,16 @@ def extract_peptides(amino_acids, scores, threshold=0.5, min_len=8, max_len=15):
     return peptides
 
 
-peptides = extract_peptides(amino_acids, scores)
+def main():
+    args = get_cli_args() 
+    amino_acids, scores = read_results(args.filename)
 
-# for length, seq in peptides:
-#     print(f"{length}-mer: {seq}")
-#
-# # Save peptides to FASTA file
-# i = 1
-# with open("peptides.fsa", "w") as file:
-#     for length, seq in peptides:
-#         file.write(f">{i}\n{seq}\n")
-#         i += 1
+    peptides = extract_peptides(amino_acids, scores)
 
-with open("peptides.pep", "w") as file:
-    for length, seq in peptides:
-        file.write(f"{seq}\n")
+    with open(args.output, "w") as file:
+        for length, seq in peptides:
+            file.write(f"{seq}\n")
+
+
+if __name__ == "__main__":
+    main()
