486805e0aa5867e0eb6657282ccf393c8a149bee
Paulo Miguel <paulomiguel@Epitopea-L031.local>
2025-09-21 00:52:22 -0400

    modify: Modified netMHCpan_results.py util function
    netMHCpan_results.py
    - Add command line argument parsing
    - Clean up code and documentation

--- utils/netMHCpan_results.py
+++ utils/netMHCpan_results.py
@@ -1,4 +1,24 @@
+import argparse
+import pandas as pd
 
+def get_cli_args():
+
+    parser = argparse.ArgumentParser(
+        description='Extract weak and strong binding pMHC from NetMHCpan results'
+    )
+
+    parser.add_argument('-f', '--filename',
+        required=True,
+        type=str,
+        help='Input results file from NetMHCpan path and name'                    
+        )
+    
+    parser.add_argument('-o', '--output',
+        required=True,
+        type=str,
+        help='Output file path and name')
+    
+    return parser.parse_args()
 
 def import_results(file):
 
@@ -13,6 +33,18 @@ def import_results(file):
 
 
 def find_binders(results):
+    """
+    Creates a list of lists containing the Peptide, HLA Allele and qualitative
+    binding strength (WB = Weak Binder, SB = Strong Binder).
+
+    Input:
+    results (list of lists): results list of lists containing NetMHCpan 4.1 
+        binding affinity predictions
+
+    Returns:
+    binders (list of lists): list of lists containing only strong or weak
+        binders
+    """
 
     binders = []
     for result in results:
@@ -21,4 +53,38 @@ def find_binders(results):
 
     return binders
 
-print(find_binders(import_results('result.txt')))
+
+def export(binders, filename):
+    """
+    Export weak and strong binders to csv
+
+    Input:
+    binders (list of lists): list of lists containing only strong or weak
+        binders
+    filename (string): File name for export CSV
+
+    Returns:
+    None
+    """
+
+    binders_df = pd.DataFrame(binders)
+
+    binders_df.to_csv(filename, index=False, header=['Peptide', 'Allele', 'Strength'])
+
+
+def main():
+    # Get command line argments
+    args = get_cli_args()
+
+    # Import results
+    results = import_results(args.filename)
+    
+    # Extract only weak or strong binders
+    binders = find_binders(results) 
+
+    # Export to CSV
+    export(binders, args.output)
+
+
+if __name__ == "__main__":
+    main()
